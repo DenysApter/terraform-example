814ae3f079f5d1ab72e67612df65322815f3e464
denisapter <denisapter@gmail.com>
2021-01-05 20:56:07 +0200

    commit message

--- README.md
+++ README.md
@@ -7,11 +7,9 @@ aws dynamodb list-tables --endpoint-url=http://localhost:4566
 aws dynamodb put-item --table-name test_table --item '{"id": {"S": "12"}, "name": {"S": "Joe" }}' --return-consumed-capacity TOTAL --endpoint-url=http://localhost:4566 
 aws dynamodb get-item --table-name test_table --key '{"id": {"S": "12"}}' --return-consumed-capacity TOTAL --endpoint-url=http://localhost:4566
 
-http://localhost:4566/restapis//local/user_request/ 
-curl http://localhost:4566/restapis/uhr8ubh4c5/local/user_request/resource 
-curl http://localhost:4566/restapis///user_request/
+curl http://localhost:4566/restapis/2n20t995ai/local/_user_request_/resource
 
-http://localhost:4566/restapis/v68nz25tz1/local/user_request/mydemoresource
+
 
 terraform init - download dependencies for aws and etc... 
 terraform plan - check what will be changed after run of terraform scripts 
--- my-lambda/src/main/java/handler/HelloLambdaHandler.java
+++ my-lambda/src/main/java/handler/HelloLambdaHandler.java
@@ -1,12 +1,20 @@
 package handler;
 
+import com.amazonaws.SDKGlobalConfiguration;
+import com.amazonaws.auth.AWSCredentialsProviderChain;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
+import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.services.dynamodbv2.document.DynamoDB;
 import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.Table;
 import com.amazonaws.services.dynamodbv2.document.spec.GetItemSpec;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ScanRequest;
+import com.amazonaws.services.dynamodbv2.model.ScanResult;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
@@ -17,28 +25,39 @@ public class HelloLambdaHandler implements RequestHandler<Map<String, Object>, S
 
     @Override
     public String handleRequest(Map<String, Object> inputParam, Context context) {
+        System.setProperty(SDKGlobalConfiguration.DISABLE_CERT_CHECKING_SYSTEM_PROPERTY, "true");
 
         LambdaLogger logger = context.getLogger();
         logger.log(inputParam.toString());
 
-        AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
+        AmazonDynamoDB client = AmazonDynamoDBClient.builder()
                 .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration("http://localhost:4566", "us-west-2"))
+                .withCredentials(new AWSStaticCredentialsProvider(new BasicAWSCredentials("key", "secret")))
                 .build();
 
-        DynamoDB dynamoDB = new DynamoDB(client);
-        Table table = dynamoDB.getTable("test_table");
+        ScanRequest scanRequest = new ScanRequest()
+                .withTableName("test_table");
 
-        GetItemSpec spec = new GetItemSpec().withPrimaryKey("id", "12");
-        Item outcome = null;
-        try {
-            System.out.println("Attempting to read the item...");
-            outcome = table.getItem(spec);
-            System.out.println("GetItem succeeded: " + outcome);
-        } catch (Exception e) {
-            System.err.println("Unable to read item");
-            System.err.println(e.getMessage());
+        ScanResult result = client.scan(scanRequest);
+        for (Map<String, AttributeValue> item : result.getItems()) {
+            System.out.println(item);
         }
+        return result.getItems().toString();
 
-        return outcome == null ? "No item" : outcome.toJSON();
+//        DynamoDB dynamoDB = new DynamoDB(client);
+//        Table table = dynamoDB.getTable("test_table");
+//
+//        GetItemSpec spec = new GetItemSpec().withPrimaryKey("id", "12");
+//        Item outcome = null;
+//        try {
+//            System.out.println("Attempting to read the item...");
+//            outcome = table.getItem(spec);
+//            System.out.println("GetItem succeeded: " + outcome);
+//        } catch (Exception e) {
+//            System.err.println("Unable to read item");
+//            System.err.println(e.getMessage());
+//        }
+//
+//        return outcome == null ? "No item" : outcome.toJSON();
     }
 }
